826eafc9fa46e4d08d19e7e024fa8f873c652a54
Nick DeRobertis <whoopnip@gmail.com>
2021-03-06 17:01:17 -0500

    Rename python file plugin folder in preparation for adding JS

--- tests/config.py
+++ tests/config.py
@@ -7,7 +7,7 @@ TESTS_PATH = Path(__file__).parent
 INPUT_FILES_PATH = TESTS_PATH / 'input_files'
 PROJECT_1_PATH = INPUT_FILES_PATH / 'project_1'
 PROJECT_1_PLUGINS_PATH = PROJECT_1_PATH / 'plugins'
-PROJECT_1_FS_PLUG_PATH = PROJECT_1_PLUGINS_PATH / 'myfsplug'
+PROJECT_1_FS_PLUG_PATH = PROJECT_1_PLUGINS_PATH / 'mypythonplug'
 
 EXPECT_PROJECT_1_PYTHON_META_DICT: Dict[str, FilePluginMeta] = {
     'on_nothing' : FilePluginMeta(PROJECT_1_FS_PLUG_PATH / "on_nothing.py"),
